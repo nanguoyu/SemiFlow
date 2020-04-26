627ee067e2074754f03ce341619ba3491dbcb21e
Dong <5096749+nanguoyu@users.noreply.github.com>
2020-04-26 15:48:48 +0200

    Update Test File and travis CI

--- tests/test_compute_gradients.py
+++ tests/test_compute_gradients.py
@@ -4,46 +4,45 @@
 @Date : 2020/4/12
 """
 from SemiFlow.engine import *
-from SemiFlow.activations import Sigmoid, ReLU
 from SemiFlow.losses import ReduceSum
 from SemiFlow.GradientDescentOptimizer import GradientDescentOptimizer
 from SemiFlow.engine.session import Session
 import numpy as np
 import matplotlib.pyplot as plt
 
-input_x = np.linspace(-1, 1, 100)
-input_y = input_x * 3 + np.random.randn(input_x.shape[0]) * 0.5
-# plt.scatter(input_x, input_y)
-# plt.show()
-# Placeholder for training data
-x = Placeholder(name='x')
-y_ = Placeholder(name='y_')
-w = Variable(1.0, name='w')
-b = Variable(0.0, name='b')
-y = x * w + b
-y.name = 'y'
 
-d = y - y_
-d.name = 'y-y_'
+def test_compute_gradients():
+    input_x = np.linspace(-1, 1, 100)
+    input_y = input_x * 3 + np.random.randn(input_x.shape[0]) * 0.5
 
-loss = ReduceSum(Square(d, name='Square'), name='loss')
+    x = Placeholder(name='x')
+    y_ = Placeholder(name='y_')
+    w = Variable(1.0, name='w')
+    b = Variable(0.0, name='b')
+    y = x * w + b
+    y.name = 'y'
 
-train_op = GradientDescentOptimizer(learning_rate=0.005, name='GD').minimize(loss)
-feed_dict = {x: input_x, y_: input_y}
+    d = y - y_
+    d.name = 'y-y_'
 
-with Session() as sess:
-    for step in range(20):
-        loss_value = sess.run(loss, feed_dict=feed_dict)
-        mse = loss_value / len(input_x)
-        print('step: {}, loss: {}, mse: {}'.format(step, loss_value, mse))
-        sess.run(train_op, feed_dict)
-    w_value = sess.run(w, feed_dict=feed_dict)
-    b_value = sess.run(b, feed_dict=feed_dict)
-    print('w: {}, b: {}'.format(w_value, b_value))
+    loss = ReduceSum(Square(d, name='Square'), name='loss')
 
-w_value = float(w_value)
-max_x, min_x = np.max(input_x), np.min(input_x)
-max_y, min_y = w_value * max_x + b_value, w_value * min_x + b_value
-plt.plot([max_x, min_x], [max_y, min_y], color='r')
-plt.scatter(input_x, input_y)
-plt.show()
+    train_op = GradientDescentOptimizer(learning_rate=0.005, name='GD').minimize(loss)
+    feed_dict = {x: input_x, y_: input_y}
+
+    with Session() as sess:
+        for step in range(20):
+            loss_value = sess.run(loss, feed_dict=feed_dict)
+            mse = loss_value / len(input_x)
+            print('step: {}, loss: {}, mse: {}'.format(step, loss_value, mse))
+            sess.run(train_op, feed_dict)
+        w_value = sess.run(w, feed_dict=feed_dict)
+        b_value = sess.run(b, feed_dict=feed_dict)
+        print('w: {}, b: {}'.format(w_value, b_value))
+
+    w_value = float(w_value)
+    max_x, min_x = np.max(input_x), np.min(input_x)
+    max_y, min_y = w_value * max_x + b_value, w_value * min_x + b_value
+    plt.plot([max_x, min_x], [max_y, min_y], color='r')
+    plt.scatter(input_x, input_y)
+    plt.show()
--- tests/test_feedforward.py
+++ tests/test_feedforward.py
@@ -3,30 +3,36 @@
 @Author: Dong Wang
 @Date : 2020/4/6
 """
+import pytest
+
 from SemiFlow.engine import *
-from SemiFlow.activations import Sigmoid, ReLU
-# with Graph().as_default():
-#     A = Variable([[1, 0], [0, -1]])
-#     b = Variable([1, 1])
-#
-#     x = Variable([1, 1])
-#
-#     y = MatMul(A, x)
-#
-#     z = Add(y, b)
-#
-#     with Session() as session:
-#         result = session.run(z)
-#         print(result)
 
-with Graph().as_default():
-    A = Variable([[1, 0], [0, -1]])
-    x = Variable([1, 1])
-    b = Variable([1, 1])
 
-    y = A.dot(x)
+def test_basic_operations():
+    with Graph().as_default():
+        A = Variable([[1, 0], [0, -1]])
+        b = Variable([1, 1])
 
-    z = y + b
-    with Session() as session:
-        result = session.run(z)
-        print(result)
+        x = Variable([1, 1])
+
+        y = MatMul(A, x)
+
+        z = Add(y, b)
+
+        with Session() as session:
+            result = session.run(z)
+            print(result)
+
+
+def test_overloading_operations():
+    with Graph().as_default():
+        A = Variable([[1, 0], [0, -1]])
+        x = Variable([1, 1])
+        b = Variable([1, 1])
+
+        y = A.dot(x)
+
+        z = y + b
+        with Session() as session:
+            result = session.run(z)
+            print(result)
