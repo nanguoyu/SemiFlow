b4072a49b7d96f21610f6e683e90505b0d7eaddf
Dong <5096749+nanguoyu@users.noreply.github.com>
2020-10-05 23:24:36 +0200

    Support cifar10 dataset and constrain Python<3.8 to avoid MemoryOver problem with unknown reason when running in py3.8

--- SemiFlow/utils/dataset.py
+++ SemiFlow/utils/dataset.py
@@ -7,6 +7,7 @@ from .download import download
 import sys
 import gzip
 import os
+import tarfile
 import pickle
 from ..engine import backend
 
@@ -31,6 +32,60 @@ def mnist(one_hot=False):
         test_set = (test_set[0], get_one_hot(test_set[1], 10))
 
     return train_set, valid_set, test_set
+
+
+def cifar10(one_hot=False):
+    url = "http://www.cs.toronto.edu/~kriz/"
+    file = "cifar-10-python.tar.gz"
+    checksum = "c58f30108f718f92721af3b95e74349a"
+    try:
+        download(url + file, file, checksum)
+    except Exception as e:
+        print("Download cifar10 failed")
+        sys.exit(1)
+
+    # load the dataset
+    train_x, train_y = [], []
+    test_x, test_y = [], []
+    with open(file, "rb") as f:
+        tar = tarfile.open(fileobj=f)
+        for item in tar:
+            obj = tar.extractfile(item)
+            if not obj or item.size < 100:
+                continue
+            cont = pickle.load(obj, encoding="bytes")
+            item_name = item.name.split("/")[-1]
+            if item_name.find('_batch_') > 0:
+                train_x.append(cont[b'data'])
+                train_y.extend(cont[b'labels'])
+            elif item_name == 'test_batch':
+                test_x = cont[b'data']
+                test_y = backend.asarray(cont[b'labels'])
+
+    train_x = backend.concatenate(train_x, axis=0)
+    # normalize
+    means, stds = (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+    train_x = train_x.astype(dtype='float32')
+    train_x = train_x / 255.0
+    train_x = train_x.reshape((-1, 1024, 3))
+    for c in range(3):
+        train_x[:, :, c] = (train_x[:, :, c] - means[c]) / stds[c]
+    train_x = train_x.reshape(-1, 3072)
+
+    train_y = backend.asarray(train_y)
+    train_set = (train_x, train_y)
+
+    test_x = test_x / 255.0
+    test_x = test_x.reshape((-1, 1024, 3))
+    for c in range(3):
+        test_x[:, :, c] = (test_x[:, :, c] - means[c]) / stds[c]
+    test_x = test_x.reshape(-1, 3072)
+    test_set = (test_x, test_y)
+
+    if one_hot:
+        train_set = (train_set[0], get_one_hot(train_set[1], 10))
+        test_set = (test_set[0], get_one_hot(test_set[1], 10))
+    return train_set, test_set
 
 
 def get_one_hot(targets, nb_classes):
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -5,13 +5,26 @@
 """
 from SemiFlow.engine import backend
 from SemiFlow.utils import BatchSpliter
-from SemiFlow.utils.dataset import mnist
+from SemiFlow.utils.dataset import mnist, cifar10
 
 
 def test_mnist():
     train_set, valid_set, test_set = mnist(one_hot=True)
     print(train_set[0].shape, train_set[1].shape)
     print(valid_set[0].shape, valid_set[1].shape)
+    print(test_set[0].shape, test_set[1].shape)
+    spliter = BatchSpliter(train_set[0], train_set[1], batch_size=128)
+    i = 0
+    for xbatch, ybatch in spliter.get_batch():
+        print("Batch ", i, " size: ", xbatch.shape, ybatch.shape)
+        print(backend.argmax(ybatch[0]))
+        i += 1
+
+
+def test_cifar10():
+    cifar10()
+    train_set, test_set = cifar10(one_hot=True)
+    print(train_set[0].shape, train_set[1].shape)
     print(test_set[0].shape, test_set[1].shape)
     spliter = BatchSpliter(train_set[0], train_set[1], batch_size=128)
     i = 0
