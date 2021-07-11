f3b946e91710ea554d65edf1262d83be1d97a0e4
Dong Wang <(dongwang@wangdongdong.wang)>
2021-07-11 23:39:16 +0200

    Update Python requirement
    Signed-off-by: Dong Wang <dongwang@wangdongdong.wang>

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=['numpy', 'matplotlib'],
     setup_requires=["numpy>=1.14.0"],
-    python_requires='>=3.6, <3.9'
+    python_requires='>=3.8'
 )
